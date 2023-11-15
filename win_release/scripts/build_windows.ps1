90e5a73d15e21937b9fb7aea475a049fa3761e77
Brad Martin <brad.martin@teledyne.com>
2023-11-15 14:48:31 -0500

    automate a windows zip release

---
+++ win_release/scripts/build_windows.ps1
@@ -0,0 +1,26 @@
+$currentDir = Get-Location
+$currentDirName = (Get-Item $currentDir).FullName
+$buildDir = (Get-Item $currentDir).Parent.FullName
+$rootDir = (Get-Item $currentDir).Parent.Parent.FullName
+
+# Get a list of all directories in the parent directory
+$oldBuildDirs = Get-ChildItem -Path $buildDir -Directory
+
+# Delete relevant directories in the parent directory
+$delDirList = @("bin", "json_library")
+foreach ($dir in $oldBuildDirs) {
+    if ($delDirList.Contains($dir.BaseName)) {
+        Remove-Item -Path $dir.FullName -Recurse -Force
+    }
+}
+
+# Build flutter and copy build
+Start-Process -FilePath "flutter" -ArgumentList "build windows --release" -WorkingDirectory (Join-Path -Path $rootDir -ChildPath "UI") -NoNewWindow -Wait
+Copy-Item -Force -Recurse (Join-Path -Path $rootDir -ChildPath "UI\build\windows\runner\Release") -Destination $buildDir
+Rename-Item -Path (Join-Path -Path $buildDir -ChildPath "Release") -NewName "bin"
+
+# Copy C++ libs
+Copy-Item -Force -Recurse -Verbose (Join-Path -Path $rootDir -ChildPath "json_library") -Destination $buildDir
+
+# Zip dir
+Compress-Archive -Force -Path $buildDir -DestinationPath (Join-Path -Path $rootDir -ChildPath "template_app_win_x64.zip")
